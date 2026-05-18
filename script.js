55bc850ebb1272a52a7d7cf942fb01f02eaea9d7
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-05-18 23:23:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function stringChop(str, size) {
-  // your code here
+  if (str === null) return [];
+  
+  const chunks = [];
+  const chunkSize = Number(size);
+  
+  for (let i = 0; i < str.length; i += chunkSize) {
+    chunks.push(str.slice(i, i + chunkSize));
+  }
+  
+  return chunks;
 }
 
 // Do not change the code below
